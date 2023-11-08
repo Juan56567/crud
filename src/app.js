9b7cf7550cdad55e0c0b68ae2d0b47907457af4b
Juan <ingenieriaespecializadahyt@gmail.com>
2023-11-08 16:58:36 -0500

    Conexion exitosa a la DB

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const morgan = require("morgan");
 const app = express();
 const path = require("path");
 const mysql = require("mysql");
+const customerRoutes = require('./routes/customer')
+
+
+
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
@@ -12,16 +16,25 @@ app.set("views", path.join(__dirname, "views"));
 // middleware
 app.use(morgan("dev"));
 app.use(
-  expressMyconnection(mysql, {
-    host: "localhost",
-    user: "root",
-    password: "Lenovo2023",
-    port: 3306,
-    database: "crud",
-  }, 'single')
+  expressMyconnection(
+    mysql,
+    {
+      host: "localhost",
+      user: "root",
+      password: "root1234",
+      port: 3306,
+      database: "crud",
+    },
+    "single"
+  )
 );
 
-//
+//routes
+app.use
+
+
+
+//starting server
 
 app.listen(3000, () => {
   console.log("Server en el puerto 3000");
