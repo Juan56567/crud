4773320ce8f83ed7222b1d32e442593fd3db1d8d
Juan <ingenieriaespecializadahyt@gmail.com>
2023-11-08 12:15:24 -0500

    se esta verificando la conexion con la DB

---
+++ database/db.sql
@@ -0,0 +1,20 @@
+
+
+CREATE DATABASE crud;
+
+
+
+use crud;
+
+CREATE TABLE customer (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    phone VARCHAR(15)
+
+);
+
+
+SHOW TABLES;
+
+decribe customer;
--- src/app.js
+++ src/app.js
@@ -1,5 +1,28 @@
 const express = require("express");
+const expressMyconnection = require("express-myconnection");
+const morgan = require("morgan");
 const app = express();
+const path = require("path");
+const mysql = require("mysql");
 
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.listen()
+// middleware
+app.use(morgan("dev"));
+app.use(
+  expressMyconnection(mysql, {
+    host: "localhost",
+    user: "root",
+    password: "Lenovo2023",
+    port: 3306,
+    database: "crud",
+  }, 'single')
+);
+
+//
+
+app.listen(3000, () => {
+  console.log("Server en el puerto 3000");
+});
